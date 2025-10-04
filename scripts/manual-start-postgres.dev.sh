e8586e4889e7453c8e87b06e68d66ef38675630d
Franco <fdescalzo@mail.austral.edu.ar>
2025-10-04 20:28:07 -0300

    chore: update Gradle build configuration and dependencies

--- scripts/manual-start-postgres.dev.sh
+++ scripts/manual-start-postgres.dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run --name=postgres_db --rm -p 5432:5432 \
+docker run --name=postgres_db_execution --rm -p 5432:5432 \
   -e POSTGRES_PASSWORD=password \
   -e POSTGRES_USER=sa \
   -e POSTGRES_DB=execution-db \
