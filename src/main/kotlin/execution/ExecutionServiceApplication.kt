b569c6d68d63852868a159889c7d0d109d7bfb2e
Franco <fdescalzo@mail.austral.edu.ar>
2025-10-07 18:36:44 -0300

    chore: update Gradle build configuration and dependencies

---
+++ src/main/kotlin/execution/ExecutionServiceApplication.kt
@@ -0,0 +1,12 @@
+package execution
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExecutionServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExecutionServiceApplication>(*args)
+}
+
