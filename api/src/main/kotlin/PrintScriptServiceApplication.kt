e543987eb846b522542afa33adc93344619560bd
Tomas093 <tjmonteiro@mail.austral.edu.ar>
2025-11-05 20:57:07 -0300

    feat: add 'component' package to scanBasePackages in PrintScriptServiceApplication

--- api/src/main/kotlin/PrintScriptServiceApplication.kt
+++ api/src/main/kotlin/PrintScriptServiceApplication.kt
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
     scanBasePackages = [
         "api", "assets", "services",
         "repositories", "helpers", "controllers",
-        "dtos", "entities", "security", "events",
+        "dtos", "entities", "security", "events", "component",
     ],
 )
 @EnableJpaRepositories(basePackages = ["repositories"])
