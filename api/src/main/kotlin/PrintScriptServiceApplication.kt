1e02074f28cc8ac0aebc55c756c1637b1c45c905
Tomas093 <tjmonteiro@mail.austral.edu.ar>
2025-11-05 20:41:33 -0300

    feat: add AppConfig for RestTemplate bean and update dependencies in build.gradle

--- api/src/main/kotlin/PrintScriptServiceApplication.kt
+++ api/src/main/kotlin/PrintScriptServiceApplication.kt
@@ -2,8 +2,6 @@ package api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.web.client.RestTemplate
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
@@ -16,10 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 )
 @EnableJpaRepositories(basePackages = ["repositories"])
 @EntityScan(basePackages = ["entities"])
-class PrintScriptServiceApplication {
-    @Bean
-    fun restTemplate(): RestTemplate = RestTemplate()
-}
+class PrintScriptServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<PrintScriptServiceApplication>(*args)
