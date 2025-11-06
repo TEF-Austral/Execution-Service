f43ad864938399da7ff9d63a4478705b05b77d86
Tomas093 <tjmonteiro@mail.austral.edu.ar>
2025-11-05 23:26:33 -0300

    refactor: disable @Component annotation in AsyncTaskResultProducer

--- events/src/main/kotlin/producers/AsyncTaskResultProducer.kt
+++ events/src/main/kotlin/producers/AsyncTaskResultProducer.kt
@@ -6,9 +6,8 @@ import result.FormattingResultEvent
 import result.LintingResultEvent
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Component
 
-@Component
+// @Component
 class AsyncTaskResultProducer(
     @param:Autowired private val producer: RedisStreamProducer,
 ) {
