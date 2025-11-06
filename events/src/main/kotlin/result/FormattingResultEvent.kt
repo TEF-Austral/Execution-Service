8d7afc8c1776fb904afd835c86e85a5393a5d260
Tomas093 <tjmonteiro@mail.austral.edu.ar>
2025-11-06 12:19:19 -0300

    feat: implement executeAllTests method to execute multiple tests and return results; add AllTestSnippetExecution data class

--- events/src/main/kotlin/result/FormattingResultEvent.kt
+++ events/src/main/kotlin/result/FormattingResultEvent.kt
@@ -2,7 +2,6 @@ package result
 
 data class FormattingResultEvent(
     val requestId: String,
-    val snippetId: Long,
     val success: Boolean,
     val formattedContent: String?,
     val error: String?,
