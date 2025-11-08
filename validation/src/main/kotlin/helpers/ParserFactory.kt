4731f55481f81e56fcfd75353daf944288a25f62
Franco <fdescalzo@mail.austral.edu.ar>
2025-11-08 19:23:43 -0300

    chore: update Gradle build configuration and dependencies

--- validation/src/main/kotlin/helpers/ParserFactory.kt
+++ validation/src/main/kotlin/helpers/ParserFactory.kt
@@ -20,13 +20,7 @@ object ParserFactory {
     ): ParserInterface {
         val reader = BufferedReader(InputStreamReader(src, StandardCharsets.UTF_8))
         val lexerFactory = DefaultLexerFactory(StringSplitterFactory, StringToTokenConverterFactory)
-        val adaptedVersion =
-            when (version) {
-                "1.1.0" -> StringToPrintScriptVersion().transform("1.1")
-                "1.0.0" -> StringToPrintScriptVersion().transform("1.0")
-                else -> StringToPrintScriptVersion().transform(version)
-            }
-
+        val adaptedVersion = StringToPrintScriptVersion().transform(version)
         val lexer = lexerFactory.createLexerWithVersion(adaptedVersion, reader)
         val tokens = LexerTokenStream(lexer)
         return DefaultParserFactory.createWithVersion(adaptedVersion, DefaultNodeBuilder(), tokens)
