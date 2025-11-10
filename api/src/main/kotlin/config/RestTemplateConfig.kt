b9f6c27625a55eeaddd96c0e9a5c04a1bb9a42ff
Franco <fdescalzo@mail.austral.edu.ar>
2025-11-09 23:20:57 -0300

    chore: update Gradle build configuration and dependencies

--- api/src/main/kotlin/config/RestTemplateConfig.kt
+++ api/src/main/kotlin/config/RestTemplateConfig.kt
@@ -1,12 +1,7 @@
 package api.config
 
-import org.slf4j.MDC
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpRequest
-import org.springframework.http.client.ClientHttpRequestExecution
-import org.springframework.http.client.ClientHttpRequestInterceptor
-import org.springframework.http.client.ClientHttpResponse
 import org.springframework.web.client.RestTemplate
 
 @Configuration
@@ -17,19 +12,5 @@ class RestTemplateConfig {
         val restTemplate = RestTemplate()
         restTemplate.interceptors.add(RequestIdPropagationInterceptor())
         return restTemplate
-    }
-}
-
-class RequestIdPropagationInterceptor : ClientHttpRequestInterceptor {
-    override fun intercept(
-        request: HttpRequest,
-        body: ByteArray,
-        execution: ClientHttpRequestExecution,
-    ): ClientHttpResponse {
-        val requestId = MDC.get("requestId")
-        if (requestId != null) {
-            request.headers.set("X-Request-ID", requestId)
-        }
-        return execution.execute(request, body)
     }
 }
